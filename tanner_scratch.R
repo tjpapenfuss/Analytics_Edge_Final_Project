953c17a32ed063f78836dcfd8272403495d4c789
Tanner Papenfuss <tannerpapenfuss@dhcp-10-29-87-252.dyn.mit.edu>
2022-10-25 14:22:16 -0400

    initial testing on tanner scratch.R

--- tanner_scratch.R
+++ tanner_scratch.R
@@ -8,26 +8,64 @@ library(caTools)
 library(dplyr)
 library(randomForest)
 library(gdata)
-library(ggplot2)
 
-ebay.df <- read.xls("eBayAuctions.xls")
-table(ebay.df$Competitive.)
+# You can use the below code however I highly recommend using the full dataset.
+# The full dataset is too big to load into GIT. You must store it locally!
+# Import the beer reviews data set. Below I am using the truncated version.
+# In the future we should update this to include all beers. 
+# beer.df <- read.csv("beer_reviews_truncated.csv")
 
-ebay.df$category.factor <- as.factor(ebay.df$Category)
-ebay.df$currency.factor <- as.factor(ebay.df$currency)
-ebay.df$endDay.factor <- as.factor(ebay.df$endDay)
-ebay.df$duration.factor <- as.factor(ebay.df$Duration)
-ebay.df$competitive.factor <- as.factor(ebay.df$Competitive.)
+# The below code reads in all beers. 
+# You must put this csv file in your own directory. 
+# DO NOT PUT THE CSV IN THE GITHUB FOLDER. You will not be able to upload it. 
+beer.df = read.csv("~/AE-FinalProj-data/beer_reviews.csv")
+count(beer.df)
+# The below code will remove the NaNs. 
+beer.df = na.omit(beer.df)
+beer.df$beer_style.factor = as.factor(beer.df$beer_style)
+
+summary(beer.df)
+str(beer.df)
+
+#A question we need to ask. Do we want to split the data on the beer styles?
+#Or do we split data based on the brewery? How do we want to present the data?
+set.seed(123)
+split = createDataPartition(beer.df$beer_style.factor, p = 0.7, list = FALSE) 
+beer.train = beer.df[split,]
+beer.test = beer.df[-split,]
+head(beer.train)
 
 
-
-set.seed(15071)
-split = createDataPartition(ebay.df$category.factor, p = 0.6, list = FALSE) 
-ebay.train = ebay.df[split,]
-ebay.test = ebay.df[-split,]
-head(ebay.train)
 # str(ebay.train)
-# Testing to make sure I have an appropriate distro. 
-ggplot(aes(x=category.factor, y = ..count..), data = ebay.train) +
+# Plotting the beer style counts. This is all beer styles
+ggplot(aes(x=beer_style.factor, y = ..count..), data = beer.df) +
   geom_bar(stat = "count") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+style_count = beer.df %>% count(beer_style, sort = TRUE)
+style_count #This gives the count of the beer styles
+
+
+#Filter beer by American IPA. This gets rid of all beers except American IPA.
+beer.ipa = filter(beer.train, beer_style == "American IPA")
+head(beer.apa)
+
+
+# Warning this pot is very convoluted. 
+ggplot(aes(x=brewery_name, y = ..count..), data = beer.ipa) +
+  geom_bar(stat = "count") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+brewery_count = beer.ipa %>% count(brewery_name, sort = TRUE)
+brewery_count #This gives the count of the beer styles
+
+ggplot(beer.ipa, aes(x = brewery_name, y = review_overall)) + geom_point() +
+  ggtitle("Age vs. Readmission") + xlab("Age") + ylab("Readmission")
+
+regressor = lm(review_overall ~ brewery_name, data = beer.ipa)
+summary(regressor)
+
+
+
+
+
