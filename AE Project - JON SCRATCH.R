c4cd341f13d14ef909ac1d488f24b9fca710d5f4
jmonnig <jon.monnig2014@yahoo.com>
2022-11-08 09:34:37 -0500

    This is a cleaned up version for CART

--- AE Project - JON SCRATCH.R
+++ AE Project - JON SCRATCH.R
@@ -1,3 +1,4 @@
+#Importing all the packages that I need
 library("dplyr")
 library("tidyr")
 library("ggplot2")
@@ -11,12 +12,34 @@ library("caret")
 library("tidyverse")
 library("gdata")
 
+library("data.table")
 
+#Setting Directory
 getwd()
-beer.df = read.csv("beer_reviews.csv")
+beer.df = read.csv("top20brewers.csv")
+beer.df = na.omit(beer.df)
+str(beer.df)
 
-cortest.df = beer.df[c("review_overall", "review_aroma", "review_appearance",
-                     "review_palate", "review_taste", "beer_abv")]
+#beer.new = beer.df[order(beer.df$review_overall, decreasing = TRUE), ]
+#beer.new = data.table(beer.new, key = "brewery_name")
+#beer.new = beer.new[ , head(.SD,10), by = brewery_name ]
+#beer.new
+
+#Cleaning up data and checking for correlation. Believe it or not but there
+#is not that much correlation between the 'opverall_' colums.
+#cortest.df = beer.df[c("review_overall", "review_aroma", "review_appearance",
+                     #"review_palate", "review_taste", "beer_abv")]
 str(cortest.df)
+#cor(cortest.df)                                                        
 
-cor(cortest.df)                                                        
+
+
+set.seed(15071)
+split = createDataPartition(beer.df$review_overall, p = 0.75, list = FALSE) 
+beer.train = beer.df[split,]
+beer.test = beer.df[-split,]
+
+df = subset(beer.test, select = -c(beer_name, brewery_name))
+
+beer.mod = rpart(review_overall~.,  data = df, 
+                 method="class", cp=0.2)
